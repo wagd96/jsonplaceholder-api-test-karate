8b9af92cb0dbbc8cabee3427aef684bd8e5964c1
Ale Gallego <wagd96@gmail.com>
2024-10-25 14:46:09 -0500

    chore(tests): disable GET and POST test scenarios

--- src/test/java/posts/PostsRunner.java
+++ src/test/java/posts/PostsRunner.java
@@ -3,14 +3,14 @@ package posts;
 import com.intuit.karate.junit5.Karate;
 
 public class PostsRunner {
-   /* @Karate.Test
+    /*@Karate.Test
     Karate testGetPosts() {
         return Karate.run("get-posts").tags("@GetPosts").relativeTo(getClass());
-    }*/
+    }
 
     @Karate.Test
     Karate testPostPosts() {
         return Karate.run("create-posts").tags("@CreatePosts").relativeTo(getClass());
-    }
+    }*/
 
 }
